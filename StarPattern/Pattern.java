ed585463183a2f1d8f157429d338f93e713fe7bc
imtiyaz15 <mr.imtiyaz15@gmail.com>
2022-08-24 18:44:26 +0530

    NA

--- StarPattern/Pattern.java
+++ StarPattern/Pattern.java
@@ -55,15 +55,15 @@ public class Pattern{
 
         for(i=0;i<row;i++){ // traversing rows
             
-            for(j=0;j<row-1;j++){ 
-                System.out.print("sp ");
+            for(j=2*(row-i);j>=0;j--){ 
+                System.out.print(" ");
             }
             
 
-            for(int k=0;k<j;k++){
-                System.out.println(" *");
+            for(int k=0;k<=i;k++){
+              System.out.print("* ");
             }
-            System.out.println();
+             System.out.println();
         }
        
     }
