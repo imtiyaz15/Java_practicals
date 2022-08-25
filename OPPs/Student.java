45452e9a6b99d3220df31fe505dfad143ebb82ca
imtiyaz15 <mr.imtiyaz15@gmail.com>
2022-08-25 11:57:38 +0530

    Opps

---
+++ OPPs/Student.java
@@ -0,0 +1,12 @@
+class Details{
+    int id;
+    String name;
+
+}
+
+public class Student{
+    public static void main(String args[]){
+        Details dt = new Details();
+        System.out.print("ID: "+dt.id+" ,Name: "+dt.name);
+    }
+}
